e17bf2d09aadd9545659d7165f46bfe607a04cbd
Rob Letts <108027101+rob-letts@users.noreply.github.com>
2023-12-02 13:33:10 +0000

    complete day two

---
+++ two/one.ts
@@ -0,0 +1,29 @@
+// TYPES
+type Totals = { [key: string]: number };
+
+// INIT
+const text = await Deno.readTextFile("two/data/input.txt");
+const lines = text.split("\n");
+
+// MAIN
+const validIds = lines.map((line) => {
+  const [_, results] = line.split(":");
+
+  const totals: Totals = {
+    red: 0,
+    green: 0,
+    blue: 0,
+  };
+
+  results.split(/;|,/)
+    .map((item) => item.trim().split(" "))
+    .forEach(([number, colour]) => {
+      totals[colour] = Math.max(totals[colour], Number(number));
+    });
+
+  return Object.values(totals).reduce((acc, value) => acc * value);
+});
+
+// OUTPUT
+const result = validIds.reduce((acc, id) => acc + id);
+console.log(result);
