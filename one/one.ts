dda10b6cf01a9cb4fb263e5e1f0addabd59f38cc
Rob Letts <108027101+rob-letts@users.noreply.github.com>
2023-12-02 09:38:38 +0000

    day one complete

--- one/one.ts
+++ one/one.ts
@@ -1,1 +1,9 @@
-console.log("init day one, question one");
+const text = await Deno.readTextFile("one/data/input.txt");
+const lines = text.split("\n");
+const numbers = lines.map((line) => line.replace(/\D/g, ""));
+
+const res = numbers.reduce((acc, num) => {
+  return acc + Number(`${num.at(0)}${num.at(-1)}`);
+}, 0);
+
+console.log(res);
--- one/two.ts
+++ one/two.ts
@@ -1,1 +1,46 @@
-console.log("init day one, question two");
+const text = await Deno.readTextFile("one/data/input.txt");
+const lines = text.split("\n");
+
+const stringNumbers = Object.fromEntries(
+  ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"].map(
+    (str, index) => {
+      return [str, index + 1];
+    },
+  ),
+);
+
+let max = 0;
+
+for (const line of lines) {
+  let earliestIndex = line.length;
+  let latestIndex = 0;
+  let [earliestValue, latestValue] = [0, 0];
+
+  for (const [str, num] of Object.entries(stringNumbers)) {
+    const firstIndexOfStr = line.indexOf(str);
+    const firstIndexOfNum = line.indexOf(String(num));
+
+    const firstIndex = Math.min(
+      firstIndexOfStr >= 0 ? firstIndexOfStr : line.length,
+      firstIndexOfNum >= 0 ? firstIndexOfNum : line.length,
+    );
+
+    if (firstIndex < earliestIndex) {
+      earliestValue = num;
+      earliestIndex = firstIndex;
+    }
+
+    const lastIndexOfStr = line.lastIndexOf(str);
+    const lastIndexOfNum = line.lastIndexOf(String(num));
+    const lastIndex = Math.max(lastIndexOfStr, lastIndexOfNum);
+
+    if (lastIndex >= latestIndex) {
+      latestValue = num;
+      latestIndex = lastIndex;
+    }
+  }
+
+  max += Number(`${earliestValue}${latestValue}`);
+}
+
+console.log(max);
