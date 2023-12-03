00270368c04b1c33d8196634fa4363e2c19b6cf6
Rob Letts <108027101+rob-letts@users.noreply.github.com>
2023-12-03 18:46:47 +0000

    complete day three

--- three/one.ts
+++ three/one.ts
@@ -2,3 +2,72 @@
 const text = await Deno.readTextFile("three/data/input.txt");
 const lines = text.split("\n");
 
+// SETUP
+const numberArray: number[] = [];
+let currentNumber = ``;
+let isValidPart = false;
+
+// HELPERS
+function findSymbol(rowIndex: number, columnIndex: number): boolean {
+  let hasFoundSymbol = false;
+
+  [-1, 0, 1].forEach((rowOffset) => {
+    [-1, 0, 1].forEach((columnOffset) => {
+      const row = rowIndex + rowOffset;
+      const column = columnIndex + columnOffset;
+
+      const isValidRow = row >= 0 && row < lines.length;
+      const isValidColumn = isValidRow && column >= 0 &&
+        column < lines[row].length;
+
+      if (isValidRow && isValidColumn) {
+        const symbol = lines[row][column];
+
+        if (symbol !== "." && symbol.match(/\D/)) {
+          hasFoundSymbol = true;
+        }
+      }
+    });
+  });
+
+  return hasFoundSymbol;
+}
+
+// MAIN
+lines.forEach((row, rowIndex) => {
+  isValidPart = false;
+
+  row.split("").forEach((column, columnIndex) => {
+    const isNotNumber = isNaN(parseInt(column));
+
+    if (isNotNumber) {
+      if (currentNumber) {
+        if (isValidPart) {
+          numberArray.push(Number(currentNumber));
+        }
+
+        currentNumber = ``;
+      }
+
+      isValidPart = false;
+    } else {
+      currentNumber += column;
+
+      if (!isValidPart) {
+        isValidPart = findSymbol(rowIndex, columnIndex);
+      }
+    }
+
+    const isLastColumn = columnIndex === row.length - 1;
+    if (isLastColumn && currentNumber) {
+      if (isValidPart) {
+        numberArray.push(Number(currentNumber));
+      }
+
+      currentNumber = ``;
+    }
+  });
+});
+
+const result = numberArray.reduce((acc, number) => acc + number, 0);
+console.log(result);
--- three/two.ts
+++ three/two.ts
@@ -1,3 +1,89 @@
+// TYPES
+type GearMap = Map<string, number[]>;
+
 // INIT
 const text = await Deno.readTextFile("three/data/input.txt");
 const lines = text.split("\n");
+
+// SETUP
+let currentNumber = "";
+let gear = "";
+const gearMap: GearMap = new Map();
+
+// HELPERS
+function findAndSetGear(
+  map: GearMap,
+  gear: string,
+  number: number,
+) {
+  const currentNumbers = map.get(gear);
+  if (currentNumbers) {
+    currentNumbers.push(number);
+  } else {
+    map.set(gear, [number]);
+  }
+}
+
+function updateGear(rowIndex: number, columnIndex: number) {
+  [-1, 0, 1].forEach((rowOffset) => {
+    [-1, 0, 1].forEach((columnOffset) => {
+      const row = rowIndex + rowOffset;
+      const column = columnIndex + columnOffset;
+
+      const isValidRow = row >= 0 && row < lines.length;
+      const isValidColumn = isValidRow && column >= 0 &&
+        column < lines[row].length;
+
+      if (isValidRow && isValidColumn) {
+        const symbol = lines[row][column];
+
+        if (symbol === "*") {
+          gear = `${row}-${column}`;
+        }
+      }
+    });
+  });
+}
+
+// MAIN
+lines.forEach((row, rowIndex) => {
+  gear = "";
+  currentNumber = "";
+
+  row.split("").forEach((column, columnIndex) => {
+    const isNotNumber = isNaN(parseInt(column));
+
+    if (isNotNumber) {
+      if (currentNumber && gear) {
+        findAndSetGear(gearMap, gear, Number(currentNumber));
+      }
+
+      currentNumber = "";
+      gear = "";
+    } else {
+      currentNumber += column;
+
+      if (!gear) {
+        updateGear(rowIndex, columnIndex);
+      }
+    }
+
+    const isLastColumn = columnIndex === row.length - 1;
+    if (isLastColumn) {
+      if (!isNotNumber && gear) {
+        findAndSetGear(gearMap, gear, Number(currentNumber));
+      }
+    }
+  });
+});
+
+// RESULT
+let total = 0;
+gearMap.forEach((numbers) => {
+  if (numbers.length === 2) {
+    const [first, second] = numbers;
+    total += first * second;
+  }
+});
+
+console.log(total);
