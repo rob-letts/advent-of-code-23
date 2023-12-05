ba954baad2b455b48e161b5d27f739dc0941b0c6
Rob Letts <108027101+rob-letts@users.noreply.github.com>
2023-12-05 14:18:58 +0000

    day five question one

--- five/one.ts
+++ five/one.ts
@@ -1,2 +1,44 @@
 const text = await Deno.readTextFile("five/data/input.txt");
-const lines = text.split("\n");
+const [seedLine, _, ...lines] = text.split("\n");
+const [__, seeds] = seedLine.split(":").map((str) => str.trim());
+const seedNumbers = seeds.split(" ").map((str) => Number(str));
+
+const currentMapLines: string[] = [];
+
+const result = seedNumbers.map((seed) => getLocation(seed));
+
+function getLocation(seed: number): number {
+  let currentSeed = seed;
+
+  lines.forEach((line, index) => {
+    const lastLine = index === lines.length - 1;
+
+    if (line.length || lastLine) {
+      currentMapLines.push(line);
+    }
+
+    if (!line.length || lastLine) {
+      const [_, ...mappings] = currentMapLines;
+
+      mappings.forEach((mapping) => {
+        const [destination, source, range] = mapping
+          .split(" ")
+          .map((str) => Number(str));
+
+        const maxBound = source + range - 1;
+
+        if (currentSeed <= maxBound && currentSeed >= source) {
+          currentSeed = currentSeed - source + destination;
+
+          // exit the loop
+          mappings.length = 0;
+        }
+      });
+
+      currentMapLines.length = 0;
+    }
+  });
+  return currentSeed;
+}
+
+console.log(Math.min(...result));
